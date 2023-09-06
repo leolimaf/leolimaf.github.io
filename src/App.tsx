71cdcf67ea1b0a4209e22e310ae6b012ad3ee19c
Leonardo Lima <leolimaf@outlook.com>
2023-09-06 11:39:50 -0300

    deploy

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './assets/styles/globals.css'
+import Banner from './components/Banner'
+import Footer from './components/Footer'
+
+export default function App() {
+
+  return (
+    <>
+      <Banner />
+      <Footer />
+    </>
+  )
+}
---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+import { AiFillLinkedin, AiFillGithub, AiOutlineInstagram  } from 'react-icons/ai'
+
+export default function Footer() {
+    return(
+        <div id="footer" className="bg-purple-950 p-8">
+            <ul className="flex justify-center gap-7 text-4xl">
+                <li><a href="https://linkedin.com/in/leonardolimaf" target="_blank"><AiFillLinkedin /></a></li>
+                <li><a href="https://github.com/leolimaf" target="_blank"><AiFillGithub /></a></li>
+                <li><a href="https://instagram.com/leolimaf54" target="_blank"><AiOutlineInstagram /></a></li>
+            </ul>
+        </div>
+    )
+}
