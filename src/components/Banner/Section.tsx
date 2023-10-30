da0477f7435687e895df2e085f9b9d0bc61e0b6b
Leonardo Lima <leonardolimaf54@gmail.com>
2023-10-30 16:39:31 -0300

    add efeito fade in

--- src/components/Banner/Section.tsx
+++ src/components/Banner/Section.tsx
@@ -1,13 +1,24 @@
+import { useEffect, useState } from "react";
+
 interface SectionProps {
     isResumoAtivo: boolean;
   }
 
 export default function Section(props: SectionProps) {
 
+    const [isActive, setIsActive] = useState(false);
+
+    useEffect(() => {
+        setIsActive(false);
+        setTimeout(() => {
+          setIsActive(true);
+        }, 10);
+      }, [props.isResumoAtivo]);
+
     return (
         <div>
             {props.isResumoAtivo ? (
-                <div className="space-y-8 mx-auto max-w-screen-lg p-2">
+                <div className={`space-y-8 mx-auto max-w-screen-lg p-2 ${isActive ? 'animate-fade-in' : ''}`}>
                     <div className="space-y-2">
                         <h2 className="text-xl font-semibold">Sobre mim:</h2>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
@@ -19,7 +30,7 @@ export default function Section(props: SectionProps) {
                     </div>
                 </div>
             ) : (
-            <div>
+            <div className={`${isActive ? 'animate-fade-in active' : ''}`}>
                 <img src="https://avatars.githubusercontent.com/u/23489043?v=4" alt="Leonardo Lima - Perfil" className="mx-auto rounded-full w-[12rem] h-[12rem] border-2" />
                 <div className="text-center py-10">
                     <h2 className="text-4xl py-1 font-medium">Leonardo Lima</h2>
