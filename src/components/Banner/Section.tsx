748ecc4f5fac179a689b3be746f0fec038270231
Leonardo Lima <leonardolimaf54@gmail.com>
2023-10-30 18:06:40 -0300

    add texto sobre mim

--- src/components/Banner/Section.tsx
+++ src/components/Banner/Section.tsx
@@ -21,7 +21,7 @@ export default function Section(props: SectionProps) {
                 <div className={`space-y-8 mx-auto max-w-screen-lg p-2 ${isActive ? 'animate-fade-in' : ''}`}>
                     <div className="space-y-2">
                         <h2 className="text-xl font-semibold">Sobre mim:</h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+                        <p>Sou um desenvolvedor de softwares determinado em criar soluções eficientes e seguras, aplicando as melhores práticas de desenvolvimento e acompanhando as tendências tecnológicas. Intrigado em entender como as aplicações funcionam, estou sempre trabalhando em vários projetos, ainda mais porque gosto de me desafiar e adquirir novos conhecimentos.</p>
                     </div>
                     <div className="space-y-2">
                         <h2 className="text-xl font-semibold">Interesses e Objetivos:</h2>
@@ -36,7 +36,7 @@ export default function Section(props: SectionProps) {
                     <h2 className="text-4xl py-1 font-medium">Leonardo Lima</h2>
                     <h3 className="text-2xl py-1 font-thin">Desenvolvedor .NET</h3>
                     <p className="text-2xl py-1 font-thin">Confira meu perfil no&nbsp;
-                        <a className={`text-pink-500 link-github`} target="_blank" href="https://github.com/leolimaf">GitHub</a>
+                        <a className={`text-pink-500 font-medium link-github`} target="_blank" href="https://github.com/leolimaf">GitHub</a>
                     </p>
                 </div>
             </div>
